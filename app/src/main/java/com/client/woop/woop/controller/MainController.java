63f2f102d3334018eb79434d209fac963a9846d5
dernico <nico.bolender.nb@gmail.com>
2015-11-11 19:30:36 +0200

    fixed login (hopefully)

--- app/src/main/java/com/client/woop/woop/controller/MainController.java
+++ app/src/main/java/com/client/woop/woop/controller/MainController.java
@@ -1,11 +1,12 @@
 package com.client.woop.woop.controller;
 
 import com.client.woop.woop.activitys.interfaces.IMainView;
+import com.client.woop.woop.data.WoopServer;
 import com.client.woop.woop.data.interfaces.IWoopServer;
 import com.client.woop.woop.navigation.INavigation;
 
 
-public class MainController {
+public class MainController implements WoopServer.WoopServerListener {
 
     private IMainView _view;
     private INavigation _navigaiton;
@@ -19,10 +20,18 @@ public class MainController {
     }
 
     private void VerifyWoopServiceIsAvailable(){
-        boolean isSet = _woop.isServiceAdressSet();
+        _woop.isServiceAdressSet(this);
+    }
+
+    @Override
+    public void serviceFound() {
+        // We dont need to handle it here
+    }
+
+    @Override
+    public void serviceAddressSet(boolean isSet) {
         if (!isSet){
             _navigaiton.navigateFragmentSettings();
         }
     }
-
 }
