2e4325e9334b75a22af3dfa0c2ceaf567c61acb5
dernico <nico.bolender.nb@gmail.com>
2015-11-12 20:43:34 +0200

    add speech recognition

--- app/src/main/java/com/client/woop/woop/fragments/streams/SearchStreamsFragment.java
+++ app/src/main/java/com/client/woop/woop/fragments/streams/SearchStreamsFragment.java
@@ -1,7 +1,13 @@
 package com.client.woop.woop.fragments.streams;
 
 import android.app.Activity;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
 import android.os.Bundle;
+import android.speech.RecognitionListener;
+import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,13 +25,21 @@ import com.client.woop.woop.fragments.interfaces.ISearchStreamsView;
 import com.client.woop.woop.models.TuneInModel;
 import com.client.woop.woop.models.YouTubeModel;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
-public class SearchStreamsFragment extends BaseFragment implements ISearchStreamsView {
+public class SearchStreamsFragment extends BaseFragment
+        implements ISearchStreamsView,
+        RecognitionListener
+{
 
     private SearchStreamsController _controller;
     private ListView _listview;
     private EditText _text;
+    private Button _searchButton;
+    private Button _voiceButton;
+    private SpeechRecognizer _speech;
 
 
     public static SearchStreamsFragment newInstance() {
@@ -50,13 +64,26 @@ public class SearchStreamsFragment extends BaseFragment implements ISearchStream
         _listview = (ListView) v.findViewById(R.id.fragment_streams_search_results);
         _text = (EditText) v.findViewById(R.id.fragment_streams_search_query);
 
-        Button searchButton = (Button) v.findViewById(R.id.fragment_streams_search_button_search);
-        searchButton.setOnClickListener(new View.OnClickListener() {
+        _searchButton = (Button) v.findViewById(R.id.fragment_streams_search_button_search);
+        _searchButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 _controller.search();
             }
         });
+
+        _voiceButton = (Button) v.findViewById(R.id.fragment_streams_voice_button);
+        _voiceButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+                _speech.startListening(intent);
+
+                showToast("Start speaking ...");
+            }
+        });
+
 
         _listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
@@ -66,6 +93,9 @@ public class SearchStreamsFragment extends BaseFragment implements ISearchStream
         });
 
         _controller = new SearchStreamsController(this, woopServer());
+
+        _speech = SpeechRecognizer.createSpeechRecognizer(getActivity());
+        _speech.setRecognitionListener(this);
 
         return v;
     }
@@ -95,5 +125,53 @@ public class SearchStreamsFragment extends BaseFragment implements ISearchStream
     @Override
     public void hideProgress() {
         super.hideProgressbar();
+    }
+
+
+    @Override
+    public void onReadyForSpeech(Bundle params) {
+
+    }
+
+    @Override
+    public void onBeginningOfSpeech() {
+    }
+
+    @Override
+    public void onRmsChanged(float rmsdB) {
+
+    }
+
+    @Override
+    public void onBufferReceived(byte[] buffer) {
+
+    }
+
+    @Override
+    public void onEndOfSpeech() {
+    }
+
+    @Override
+    public void onError(int error) {
+
+    }
+
+    @Override
+    public void onResults(Bundle results) {
+        ArrayList<String> words = results.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+        if(words.size() > 0) {
+            _text.setText(words.get(0));
+            _controller.search();
+        }
+    }
+
+    @Override
+    public void onPartialResults(Bundle partialResults) {
+
+    }
+
+    @Override
+    public void onEvent(int eventType, Bundle params) {
+
     }
 }
